1c74629ff93aef80dcd756014a80f763e7d2fac6
luan <master@ubibi.cn>
2018-03-20 16:26:10 +0800

    es5

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var mysql = require('mysql');
 
 
 function toWhereSql(queryCondition) {
@@ -25,223 +24,219 @@ function toWhereSql(queryCondition) {
 }
 
 
-class MySQLWrapper {
+function MySQLWrapper(dbModel, connectionPool) {
+    this.dbModel = dbModel;
+    this.connectionPool = connectionPool;
+}
 
 
-    constructor(dbModel,connectionPool) {
-        this.dbModel = dbModel;
-        this.connectionPool = connectionPool;
+MySQLWrapper.prototype.getConnectionPool = function () {
+    if (this.connectionPool) {
+        return this.connectionPool;
     }
+};
 
 
-    getConnectionPool() {
-        if(this.connectionPool){
-            return this.connectionPool;
+MySQLWrapper.prototype.isTableField = function (fieldName) {
+    var tableFields = this.dbModel.tableFields;
+    for (var j = 0; j < tableFields.length; j++) {
+        var obj1 = tableFields[j];
+        if (obj1 === fieldName) {
+            return true;
         }
     }
+    return false;
+};
 
 
-    isTableField(fieldName) {
-        var tableFields = this.dbModel.tableFields;
-        for (var j = 0; j < tableFields.length; j++) {
-            var obj1 = tableFields[j];
-            if (obj1 === fieldName) {
-                return true;
+MySQLWrapper.prototype.doExecuteSql = function (requestModel) {
+
+    var sql = requestModel['sql'];
+    var params = requestModel['params'] || [];
+
+
+    const that = this;
+    return new Promise(function (resolve, reject) {
+
+        var pool = that.getConnectionPool();
+        pool.getConnection(function (err1, connection) {
+
+            if (err1) {
+                reject(err1);
+                return;
             }
-        }
-        return false;
-    }
 
+            // LogUtils.info(sql);
 
-    doExecuteSql(requestModel) {
-
-        var sql = requestModel['sql'];
-        var params = requestModel['params'] || [];
-
-
-        const that = this;
-        return new Promise(function (resolve, reject) {
-
-            var pool = that.getConnectionPool();
-            pool.getConnection(function (err1, connection) {
-
-                if (err1) {
-                    reject(err1);
-                    return;
+            connection.query(sql, params, function (err2, results, fields) {
+                connection.release();
+                if (err2) {
+                    reject(err2);
+                } else {
+                    resolve(results);
                 }
-
-                // LogUtils.info(sql);
-
-                connection.query(sql, params, function (err2, results, fields) {
-                    connection.release();
-                    if (err2) {
-                        reject(err2);
-                    } else {
-                        resolve(results);
-                    }
-                });
             });
-
         });
-    }
+
+    });
+};
 
 
-    doQueryByWhereSql(whereSql, whereValues) {
-        var tableName = this.dbModel.tableName;
-        return this.doExecuteSql({
-            sql: "select * from `" + tableName + "` where " + whereSql,
-            params: (whereValues || [])
-        });
-    }
+MySQLWrapper.prototype.doQueryByWhereSql = function (whereSql, whereValues) {
+    var tableName = this.dbModel.tableName;
+    return this.doExecuteSql({
+        sql: "select * from `" + tableName + "` where " + whereSql,
+        params: (whereValues || [])
+    });
+};
 
 
-    doQuery(queryCondition, sql_suffix) {
-        var mm = toWhereSql(queryCondition);
-        var whereSql = mm.whereSql + " " + (sql_suffix || "");
-        return this.doQueryByWhereSql(whereSql, mm.values);
-    }
+MySQLWrapper.prototype.doQuery = function (queryCondition, sql_suffix) {
+    var mm = toWhereSql(queryCondition);
+    var whereSql = mm.whereSql + " " + (sql_suffix || "");
+    return this.doQueryByWhereSql(whereSql, mm.values);
+};
 
-    doCount(queryCondition) {
-        var mm = toWhereSql(queryCondition);
-        var tableName = this.dbModel.tableName;
-        return this.doExecuteSql({
-            sql: "select count(0) from `" + tableName + "` where " + mm.whereSql,
-            params: mm.values
-        });
-    }
-
-
-    doQueryById(id) {
-        return this.doQuery({"id": id});
-    }
+MySQLWrapper.prototype.doCount = function (queryCondition) {
+    var mm = toWhereSql(queryCondition);
+    var tableName = this.dbModel.tableName;
+    return this.doExecuteSql({
+        sql: "select count(0) from `" + tableName + "` where " + mm.whereSql,
+        params: mm.values
+    });
+};
 
 
-    doQueryByName(name) {
-        return this.doQuery({"name": name});
-    }
+MySQLWrapper.prototype.doQueryById = function (id) {
+    return this.doQuery({"id": id});
+};
 
 
-    doDeleteByWhereSql(whereSql, whereValues) {
-        var tableName = this.dbModel.tableName;
-        return this.doExecuteSql({
-            sql: "delete from `" + tableName + "` where " + whereSql,
-            params: (whereValues || [])
-        });
-    }
-
-    doDelete(queryCondition) {
-        var mm = toWhereSql(queryCondition);
-        return this.doDeleteByWhereSql(mm.whereSql, mm.values);
-    }
-
-    doDeleteById(id) {
-        return this.doDelete({id: id});
-    }
+MySQLWrapper.prototype.doQueryByName = function (name) {
+    return this.doQuery({"name": name});
+};
 
 
-    doInsert(insertObject) {
+MySQLWrapper.prototype.doDeleteByWhereSql = function (whereSql, whereValues) {
+    var tableName = this.dbModel.tableName;
+    return this.doExecuteSql({
+        sql: "delete from `" + tableName + "` where " + whereSql,
+        params: (whereValues || [])
+    });
+};
 
-        insertObject['update_time'] = new Date().getTime();
-        insertObject['create_time'] = new Date().getTime();
+MySQLWrapper.prototype.doDelete = function (queryCondition) {
+    var mm = toWhereSql(queryCondition);
+    return this.doDeleteByWhereSql(mm.whereSql, mm.values);
+};
 
-        let that = this;
-        let tableName = that.dbModel.tableName;
-        var objectKeys = Object.keys(insertObject);
+MySQLWrapper.prototype.doDeleteById = function (id) {
+    return this.doDelete({id: id});
+};
 
-        var insertKeys = [];
-        var insertValues = [];
-        var insertValuesHolder = [];
 
-        for (var i = 0; i < objectKeys.length; i++) {
-            var objKey = objectKeys[i];
-            var objValue = insertObject[objKey];
+MySQLWrapper.prototype.doInsert = function (insertObject) {
 
-            if (that.isTableField(objKey)) {
-                insertKeys.push("`" + objKey + "`");
-                insertValuesHolder.push("?");
-                insertValues.push(objValue);
-            }
+    insertObject['update_time'] = new Date().getTime();
+    insertObject['create_time'] = new Date().getTime();
+
+    let that = this;
+    let tableName = that.dbModel.tableName;
+    var objectKeys = Object.keys(insertObject);
+
+    var insertKeys = [];
+    var insertValues = [];
+    var insertValuesHolder = [];
+
+    for (var i = 0; i < objectKeys.length; i++) {
+        var objKey = objectKeys[i];
+        var objValue = insertObject[objKey];
+
+        if (that.isTableField(objKey)) {
+            insertKeys.push("`" + objKey + "`");
+            insertValuesHolder.push("?");
+            insertValues.push(objValue);
         }
-
-
-        var insertKeysString = insertKeys.join(",");
-        var insertValuesHolderString = insertValuesHolder.join(",");
-
-        var sql = "INSERT INTO  `" + tableName + "` (" + insertKeysString + ") VALUES(" + insertValuesHolderString + ")";
-
-        return that.doExecuteSql({
-            sql: sql,
-            params: insertValues
-        });
     }
 
 
-    doUpdateByWhereSql(updateObject, whereSql, whereValues) {
-        updateObject['update_time'] = new Date().getTime();
+    var insertKeysString = insertKeys.join(",");
+    var insertValuesHolderString = insertValuesHolder.join(",");
 
-        let that = this;
+    var sql = "INSERT INTO  `" + tableName + "` (" + insertKeysString + ") VALUES(" + insertValuesHolderString + ")";
 
-        var objectKeys = Object.keys(updateObject);
-        var tableName = that.dbModel.tableName;
+    return that.doExecuteSql({
+        sql: sql,
+        params: insertValues
+    });
+};
 
-        var updateKeys = [];
-        var updateValues = [];
 
-        for (var i = 0; i < objectKeys.length; i++) {
-            var objKey = objectKeys[i];
-            var objValue = updateObject[objKey];
+MySQLWrapper.prototype.doUpdateByWhereSql = function (updateObject, whereSql, whereValues) {
+    updateObject['update_time'] = new Date().getTime();
 
-            if (that.isTableField(objKey)) {
-                updateKeys.push(" `" + objKey + "`=? ");
-                updateValues.push(objValue);
-            }
+    let that = this;
+
+    var objectKeys = Object.keys(updateObject);
+    var tableName = that.dbModel.tableName;
+
+    var updateKeys = [];
+    var updateValues = [];
+
+    for (var i = 0; i < objectKeys.length; i++) {
+        var objKey = objectKeys[i];
+        var objValue = updateObject[objKey];
+
+        if (that.isTableField(objKey)) {
+            updateKeys.push(" `" + objKey + "`=? ");
+            updateValues.push(objValue);
         }
-
-        var updateKeysString = updateKeys.join(",");
-
-        var sql = "update `" + tableName + "` set " + updateKeysString + "  where " + whereSql;
-
-        return that.doExecuteSql({
-            sql: sql,
-            params: updateValues.concat(whereValues)
-        });
-
     }
 
+    var updateKeysString = updateKeys.join(",");
 
-    doUpdate(updateObject, whereObject) {
-        var mm = toWhereSql(whereObject);
-        var whereSql = mm.whereSql;
-        var whereValues = mm.values;
-        return this.doUpdateByWhereSql(updateObject, whereSql, whereValues);
-    }
+    var sql = "update `" + tableName + "` set " + updateKeysString + "  where " + whereSql;
 
+    return that.doExecuteSql({
+        sql: sql,
+        params: updateValues.concat(whereValues)
+    });
 
-    doUpdateById(updateObject, id) {
-        return this.doUpdate(updateObject, {id: id});
-    }
+};
 
 
-    saveOrUpdate(updateObject, whereCondition) {
-        var result = this.doQuery(whereCondition);
+MySQLWrapper.prototype.doUpdate = function (updateObject, whereObject) {
+    var mm = toWhereSql(whereObject);
+    var whereSql = mm.whereSql;
+    var whereValues = mm.values;
+    return this.doUpdateByWhereSql(updateObject, whereSql, whereValues);
+};
+
+
+MySQLWrapper.prototype.doUpdateById = function (updateObject, id) {
+    return this.doUpdate(updateObject, {id: id});
+};
+
+
+MySQLWrapper.prototype.saveOrUpdate = function (updateObject, whereCondition) {
+    var query_result = this.doQuery(whereCondition);
+    return query_result.then(function (result) {
         if (result && result.length > 0) {
             return this.doUpdate(updateObject, whereCondition);
         } else {
             return this.doInsert(updateObject);
         }
-    }
+    });
+};
 
 
-    saveOrUpdateById(updateObject, id) {
-        return this.saveOrUpdate(updateObject, {id: id});
-    }
-
-}
-
+MySQLWrapper.prototype.saveOrUpdateById = function (updateObject, id) {
+    return this.saveOrUpdate(updateObject, {id: id});
+};
 
 
 module.exports = {
-    MySQLWrapper:MySQLWrapper
+    MySQLWrapper: MySQLWrapper
 };
 
 
